8f06d50a4aa5ccb59564ceb3858b577b6522ae39
Patxi Gortázar <patxi.gortazar@gmail.com>
2022-08-07 17:59:11 +0200

    Add initial implementation with current price

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
-# precio-luz
-Gnome shell extension for energy pricing (Spain)
+# Precio-luz
+```
+Extension to Gnome-Shell v.(36,38,40,41,42), for energy price information (Spain).
+
+# Licence
+```
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 2 of the License, or
+(at your option) any later version.
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+SPDX-License-Identifier: GPL-2.0-or-later
+```
+# How to install
+```
+cd /tmp && git clone https://github.com/gortazar/precio-luz.git && mv dollar-pkr precio-luz@gortazar.github.com && cp -av precio-luz@gortazar.github.com ~/.local/share/gnome-shell/extensions/ && gnome-shell-extension-tool --enable-extension precio-luz@gortazar.github.com && rm -rf precio-luz@gortazar.github.com .
+Or just download precio-luz@gortazar.github.com.zip form releases and unzip precio-luz@gortazar.github.com to ~/.local/share/gnome-shell/extensions/ .
+```
+To restart GNOME Shell in X11, pressing Alt+F2 to open the Run Dialog and enter restart 
+(or just r). 
+In Wayland Log out and Login agaian.
---
+++ extension.js
@@ -0,0 +1,126 @@
+/* extension.js
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * SPDX-License-Identifier: GPL-2.0-or-later
+ */
+
+'use strict';
+
+const {St, Gio, Clutter, Soup, GLib} = imports.gi;
+
+const ExtensionUtils = imports.misc.extensionUtils;
+const Me = ExtensionUtils.getCurrentExtension();
+const Main = imports.ui.main;
+const PanelMenu = imports.ui.panelMenu;
+
+let panelButton;
+let panelButtonText;
+let _httpSession;
+let _dollarQuotation;
+let sourceId = null;
+
+// Start application
+function init(){
+    log(`initializing ${Me.metadata.name}`);
+}
+
+// Add the button to the panel
+function enable() {
+    log(`enabling ${Me.metadata.name}`);
+    panelButton = new St.Bin({
+        style_class : "panel-button",
+    });
+
+    load_json_async();
+    Main.panel._rightBox.insert_child_at_index(panelButton, 0);
+    sourceId = GLib.timeout_add_seconds(GLib.PRIORITY_DEFAULT, 60, () => {
+        load_json_async();
+        return GLib.SOURCE_CONTINUE;
+    });
+}
+
+// Remove the added button from panel
+function disable(){
+    log(`disabling ${Me.metadata.name}`);
+    Main.panel._rightBox.remove_child(panelButton);
+    
+    if (panelButton) {
+        panelButton.destroy();
+        panelButton = null;
+    }   
+    if (sourceId) {
+        GLib.Source.remove(sourceId);
+        sourceId = null;
+    }
+}
+
+// Requests API Dollar
+function load_json_async(){
+    if (_httpSession === undefined) {
+        _httpSession = new Soup.Session();
+    } else {
+        _httpSession.abort();
+    }
+
+    let message = Soup.form_request_new_from_hash(
+        'GET', 
+        "https://api.preciodelaluz.org/v1/prices/now", 
+        {"zone":"PCB"});
+    
+    _httpSession.queue_message(message, () => {
+        try {
+            if (!message.response_body.data) {
+                panelButtonText = new St.Label({
+                    text : "💡 ? €/kWh)",
+                    y_align: Clutter.ActorAlign.CENTER,
+                });
+                panelButton.set_child(panelButtonText);
+                _httpSession.abort();
+                return;
+            }
+
+            let jp = JSON.parse(message.response_body.data);
+            let price = jp["price"];
+            price = price / 1000.0; // Convert from MWh to kWh
+   
+            let _style = "standard-style";
+            if(jp["is-cheap"]) {
+
+                _style = "cheap-style"
+            } else if(jp["is-under-avg"]) {
+                    _style = "under-avg-style"
+            }
+            panelButtonText = new St.Label({
+                text : "💡" + price.toFixed(3) + " €/kWh",
+                y_align: Clutter.ActorAlign.CENTER,
+                style_class: _style
+            });
+
+            panelButton.set_child(panelButtonText);
+            _httpSession.abort();
+            return;
+
+        } catch (e) {
+            panelButtonText = new St.Label({
+                text : "Luz error",
+                y_align: Clutter.ActorAlign.CENTER,
+            });
+
+            panelButton.set_child(panelButtonText);
+            _httpSession.abort();
+            return;
+        }
+    });
+}
